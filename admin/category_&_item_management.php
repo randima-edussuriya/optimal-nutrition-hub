6fe8b084c202d87e8095daf9ecdc3f19b54a7d2f
randima217 <152683475+randima217@users.noreply.github.com>
2023-12-11 18:37:28 +0530

    format

--- admin/category_&_item_management.php
+++ admin/category_&_item_management.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS link -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/home.css">
     <style>
         .admin_image {
@@ -26,9 +25,7 @@
                 <a class="navbar-brand ms-5 me-auto " href="index.html">
                     <img src="../Images/logo.svg" alt="logo" class="logo ">
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -36,8 +33,7 @@
                     <ul class="navbar-nav ms-auto ">
 
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><img src="../Icons/profile-line.svg" alt=""
-                                    style="width: 32px;"></a>
+                            <a class="nav-link" href="#"><img src="../Icons/profile-line.svg" alt="" style="width: 32px;"></a>
                         </li>
                     </ul>
                     <!-- Right item end -->
@@ -69,17 +65,15 @@
         <!-- Create Category section start -->
         <div class="container my-5">
             <?php
-        if(isset($_GET['create_category'])){
-            include('create_category.php');
-        }
-        ?>
+            if (isset($_GET['create_category'])) {
+                include('create_category.php');
+            }
+            ?>
         </div>
         <!-- Create Category section end -->
- 
+
         <!--Bootstrap JS link -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 
 </html>
--- admin/create_category.php
+++ admin/create_category.php
@@ -1,11 +1,11 @@
 <form action="" method="post" class="mb-2">
-<div class="input-group w-90 mb-2">
-  <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-  <input type="text" class="form-control" name="cat_name" placeholder="Create Category" aria-label="Username" aria-describedby="basic-addon1">
-</div>
-<div class="input-group w-10 mb-2">
-  
-  <!--type="submit" class="form-control bg-info" name="create_cat" value="Create Category"--> 
-  <button class="bg-info p-2 my-2 border-0" name="create_cat">Create Category</button>
-</div>
-</form> 
+  <div class="input-group w-90 mb-2">
+    <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+    <input type="text" class="form-control" name="cat_name" placeholder="Create Category" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+  <div class="input-group w-10 mb-2">
+
+    <!--type="submit" class="form-control bg-info" name="create_cat" value="Create Category"-->
+    <button class="bg-info p-2 my-2 border-0" name="create_cat">Create Category</button>
+  </div>
+</form>
