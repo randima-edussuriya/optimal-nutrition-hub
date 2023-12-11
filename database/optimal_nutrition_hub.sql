44d2b62f948e96d8a6581f813e522d1d005f00ce
randima217 <152683475+randima217@users.noreply.github.com>
2023-12-11 08:43:57 +0530

    add database

---
+++ database/optimal_nutrition_hub.sql
@@ -0,0 +1,522 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2023 at 04:12 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `optimal_nutrition_hub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cancellation`
+--
+
+CREATE TABLE `cancellation` (
+  `cancel_id` int(11) NOT NULL,
+  `cancel_date` datetime DEFAULT NULL,
+  `cancel_reason` text DEFAULT NULL,
+  `fk_order_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_id` int(11) NOT NULL,
+  `cart_item_qty` int(11) DEFAULT NULL,
+  `fk_item_id` int(11) DEFAULT NULL,
+  `fk_cust_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(2000, 'cat1'),
+(2001, 'cat2'),
+(2002, 'cat3'),
+(2003, 'cat4'),
+(2004, 'cat5');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cust_id` int(11) NOT NULL,
+  `cust_fname` varchar(50) DEFAULT NULL,
+  `cust_lname` varchar(50) DEFAULT NULL,
+  `cust_username` varchar(50) DEFAULT NULL,
+  `cust_pwd` varchar(50) DEFAULT NULL,
+  `cust_email` varchar(100) DEFAULT NULL,
+  `cust_is_active` tinyint(1) DEFAULT 1,
+  `cust_phone` varchar(15) DEFAULT NULL,
+  `cust_add_line1` varchar(255) DEFAULT NULL,
+  `cust_add_line2` varchar(255) DEFAULT NULL,
+  `cust_add_line3` varchar(255) DEFAULT NULL,
+  `cust_add_line4` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_notification`
+--
+
+CREATE TABLE `customer_notification` (
+  `cust_notifi_id` int(11) NOT NULL,
+  `fk_notifi_id` int(11) DEFAULT NULL,
+  `fk_cust_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deliver_area`
+--
+
+CREATE TABLE `deliver_area` (
+  `deliver_area_id` int(11) NOT NULL,
+  `deliver_area_name` varchar(50) DEFAULT NULL,
+  `fk_staff_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feedback_id` int(11) NOT NULL,
+  `feedback_date` datetime DEFAULT NULL,
+  `feedback_msg` text DEFAULT NULL,
+  `feedback_status` varchar(20) DEFAULT NULL,
+  `fk_staff_id` int(11) DEFAULT NULL,
+  `fk_item_id` int(11) DEFAULT NULL,
+  `fk_cust_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inquiry`
+--
+
+CREATE TABLE `inquiry` (
+  `inquiry_id` int(11) NOT NULL,
+  `inquiry_date` datetime DEFAULT NULL,
+  `inquiry_msg` text DEFAULT NULL,
+  `inquiry_reply` text DEFAULT NULL,
+  `inquiry_reply_date` datetime DEFAULT NULL,
+  `fk_staff_id` int(11) DEFAULT NULL,
+  `fk_cust_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL,
+  `item_name` varchar(100) DEFAULT NULL,
+  `item_image1` varchar(255) DEFAULT NULL,
+  `item_image2` varchar(255) DEFAULT NULL,
+  `item_brand` varchar(50) DEFAULT NULL,
+  `item_description` text DEFAULT NULL,
+  `item_sell_price` decimal(10,2) DEFAULT NULL,
+  `item_cost_price` decimal(10,2) DEFAULT NULL,
+  `item_stock_qty` int(11) DEFAULT NULL,
+  `item_discount` decimal(5,2) DEFAULT NULL,
+  `item_date_added` datetime DEFAULT NULL,
+  `fk_category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `notifi_id` int(11) NOT NULL,
+  `notifi_msg` text DEFAULT NULL,
+  `notifi_date` datetime DEFAULT NULL,
+  `fk_staff_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_item`
+--
+
+CREATE TABLE `order_item` (
+  `order_item_id` int(11) NOT NULL,
+  `order_item_qty` int(11) DEFAULT NULL,
+  `order_item_price` decimal(10,2) DEFAULT NULL,
+  `fk_item_id` int(11) DEFAULT NULL,
+  `fk_order_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_table`
+--
+
+CREATE TABLE `order_table` (
+  `order_id` int(11) NOT NULL,
+  `order_date` datetime DEFAULT NULL,
+  `order_total` decimal(10,2) DEFAULT NULL,
+  `order_status` varchar(20) DEFAULT NULL,
+  `order_deliver_option` varchar(50) DEFAULT NULL,
+  `fk_cust_id` int(11) DEFAULT NULL,
+  `fk_deliver_area_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `staff_id` int(11) NOT NULL,
+  `staff_fname` varchar(50) DEFAULT NULL,
+  `staff_lname` varchar(50) DEFAULT NULL,
+  `staff_username` varchar(50) DEFAULT NULL,
+  `staff_pwd` varchar(50) DEFAULT NULL,
+  `staff_email` varchar(100) DEFAULT NULL,
+  `staff_is_active` tinyint(1) DEFAULT 1,
+  `staff_phone` varchar(15) DEFAULT NULL,
+  `staff_hire_date` datetime DEFAULT NULL,
+  `staff_nic` varchar(15) DEFAULT NULL,
+  `staff_add_line1` varchar(255) DEFAULT NULL,
+  `staff_add_line2` varchar(255) DEFAULT NULL,
+  `staff_add_line3` varchar(255) DEFAULT NULL,
+  `staff_add_line4` varchar(255) DEFAULT NULL,
+  `fk_staff_type_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff_type`
+--
+
+CREATE TABLE `staff_type` (
+  `staff_type_id` int(11) NOT NULL,
+  `staff_type_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cancellation`
+--
+ALTER TABLE `cancellation`
+  ADD PRIMARY KEY (`cancel_id`),
+  ADD KEY `fk_order_id` (`fk_order_id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `fk_item_id` (`fk_item_id`),
+  ADD KEY `fk_cust_id` (`fk_cust_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cust_id`);
+
+--
+-- Indexes for table `customer_notification`
+--
+ALTER TABLE `customer_notification`
+  ADD PRIMARY KEY (`cust_notifi_id`),
+  ADD KEY `fk_notifi_id` (`fk_notifi_id`),
+  ADD KEY `fk_cust_id` (`fk_cust_id`);
+
+--
+-- Indexes for table `deliver_area`
+--
+ALTER TABLE `deliver_area`
+  ADD PRIMARY KEY (`deliver_area_id`),
+  ADD KEY `fk_staff_id` (`fk_staff_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedback_id`),
+  ADD KEY `fk_staff_id` (`fk_staff_id`),
+  ADD KEY `fk_item_id` (`fk_item_id`),
+  ADD KEY `fk_cust_id` (`fk_cust_id`);
+
+--
+-- Indexes for table `inquiry`
+--
+ALTER TABLE `inquiry`
+  ADD PRIMARY KEY (`inquiry_id`),
+  ADD KEY `fk_staff_id` (`fk_staff_id`),
+  ADD KEY `fk_cust_id` (`fk_cust_id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `fk_category_id` (`fk_category_id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`notifi_id`),
+  ADD KEY `fk_staff_id` (`fk_staff_id`);
+
+--
+-- Indexes for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD PRIMARY KEY (`order_item_id`),
+  ADD KEY `fk_item_id` (`fk_item_id`),
+  ADD KEY `fk_order_id` (`fk_order_id`);
+
+--
+-- Indexes for table `order_table`
+--
+ALTER TABLE `order_table`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `fk_cust_id` (`fk_cust_id`),
+  ADD KEY `fk_deliver_area_id` (`fk_deliver_area_id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`staff_id`),
+  ADD KEY `fk_staff_type_id` (`fk_staff_type_id`);
+
+--
+-- Indexes for table `staff_type`
+--
+ALTER TABLE `staff_type`
+  ADD PRIMARY KEY (`staff_type_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cancellation`
+--
+ALTER TABLE `cancellation`
+  MODIFY `cancel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5500;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3500;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2005;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3000;
+
+--
+-- AUTO_INCREMENT for table `customer_notification`
+--
+ALTER TABLE `customer_notification`
+  MODIFY `cust_notifi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7500;
+
+--
+-- AUTO_INCREMENT for table `deliver_area`
+--
+ALTER TABLE `deliver_area`
+  MODIFY `deliver_area_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4000;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6000;
+
+--
+-- AUTO_INCREMENT for table `inquiry`
+--
+ALTER TABLE `inquiry`
+  MODIFY `inquiry_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6500;
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2500;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `notifi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7000;
+
+--
+-- AUTO_INCREMENT for table `order_item`
+--
+ALTER TABLE `order_item`
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5000;
+
+--
+-- AUTO_INCREMENT for table `order_table`
+--
+ALTER TABLE `order_table`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4500;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1500;
+
+--
+-- AUTO_INCREMENT for table `staff_type`
+--
+ALTER TABLE `staff_type`
+  MODIFY `staff_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cancellation`
+--
+ALTER TABLE `cancellation`
+  ADD CONSTRAINT `cancellation_ibfk_1` FOREIGN KEY (`fk_order_id`) REFERENCES `order_table` (`order_id`);
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`fk_item_id`) REFERENCES `item` (`item_id`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`fk_cust_id`) REFERENCES `customer` (`cust_id`);
+
+--
+-- Constraints for table `customer_notification`
+--
+ALTER TABLE `customer_notification`
+  ADD CONSTRAINT `customer_notification_ibfk_1` FOREIGN KEY (`fk_notifi_id`) REFERENCES `notification` (`notifi_id`),
+  ADD CONSTRAINT `customer_notification_ibfk_2` FOREIGN KEY (`fk_cust_id`) REFERENCES `customer` (`cust_id`);
+
+--
+-- Constraints for table `deliver_area`
+--
+ALTER TABLE `deliver_area`
+  ADD CONSTRAINT `deliver_area_ibfk_1` FOREIGN KEY (`fk_staff_id`) REFERENCES `staff` (`staff_id`);
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`fk_staff_id`) REFERENCES `staff` (`staff_id`),
+  ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`fk_item_id`) REFERENCES `item` (`item_id`),
+  ADD CONSTRAINT `feedback_ibfk_3` FOREIGN KEY (`fk_cust_id`) REFERENCES `customer` (`cust_id`);
+
+--
+-- Constraints for table `inquiry`
+--
+ALTER TABLE `inquiry`
+  ADD CONSTRAINT `inquiry_ibfk_1` FOREIGN KEY (`fk_staff_id`) REFERENCES `staff` (`staff_id`),
+  ADD CONSTRAINT `inquiry_ibfk_2` FOREIGN KEY (`fk_cust_id`) REFERENCES `customer` (`cust_id`);
+
+--
+-- Constraints for table `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`fk_category_id`) REFERENCES `category` (`category_id`);
+
+--
+-- Constraints for table `notification`
+--
+ALTER TABLE `notification`
+  ADD CONSTRAINT `notification_ibfk_1` FOREIGN KEY (`fk_staff_id`) REFERENCES `staff` (`staff_id`);
+
+--
+-- Constraints for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD CONSTRAINT `order_item_ibfk_1` FOREIGN KEY (`fk_item_id`) REFERENCES `item` (`item_id`),
+  ADD CONSTRAINT `order_item_ibfk_2` FOREIGN KEY (`fk_order_id`) REFERENCES `order_table` (`order_id`);
+
+--
+-- Constraints for table `order_table`
+--
+ALTER TABLE `order_table`
+  ADD CONSTRAINT `order_table_ibfk_1` FOREIGN KEY (`fk_cust_id`) REFERENCES `customer` (`cust_id`),
+  ADD CONSTRAINT `order_table_ibfk_2` FOREIGN KEY (`fk_deliver_area_id`) REFERENCES `deliver_area` (`deliver_area_id`);
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`fk_staff_type_id`) REFERENCES `staff_type` (`staff_type_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
