f72c31c5d714f116e81419ef4ddf7eb80dd55cd5
randima217 <152683475+randima217@users.noreply.github.com>
2023-12-20 17:37:17 +0530

    add discount percentage and price to product cart

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <!-- Bootstrap CSS link -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="css/home.css">
+  <link rel="stylesheet" href="css/home.css?v=<?php echo time(); ?>">
   <link rel="stylesheet" href="css/footer.css">
   <title>Home-Optimal Nutrition Hub</title>
 </head>
@@ -45,11 +45,15 @@
     </div>
     <div class="row">
       <div class="col-sm-6 col-md-4 col-lg-3 text-center mb-4">
-        <div class="card border-0 bg-light ">
-          <img src="Images/products/GAT NITRAFLEX 30 SERVING.jpg" class="card-img-top p-2" alt="...">
+        <div class="card border-0 bg-light">
+          <img src="Images/products/GAT NITRAFLEX 30 SERVING.jpg" class="card-img-top p-2 position-relative" alt="...">
+          <span class="position-absolute top-0 start-0 rounded-end-pill badge bg-success p-2">
+            -12%
+          </span>
           <div class="card-body p-0 pb-2">
-            <h6>GAT NITRAFLEX 30 SERVING</h6>
-            <h6>Rs. 7500</h6>
+            <h6 class="product-name" class="product-name">GAT NITRAFLEX 30 SERVING</h6>
+            <h6 class="text-decoration-line-through d-inline text-body-tertiary">Rs. 7500</h6>
+            <h6 class="d-inline">Rs. 7500</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -59,8 +63,8 @@
         <div class="card border-0 bg-light ">
           <img src="Images/products/NUTREX CREATINE DRIVE 300G.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>NUTREX CREATINE DRIVE</h6>
-            <h6>Rs. 7500</h6>
+            <h6 class="product-name">NUTREX CREATINE DRIVE</h6>
+            <h6 class="d-inline">Rs. 7500</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -70,8 +74,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/USN VIBRANCE COLLAGEN PEPTIDES 30 SERVINGS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>USN VIBRANCE COLLAGEN</h6>
-            <h6>Rs. 11000</h6>
+            <h6 class="product-name">USN VIBRANCE COLLAGEN</h6>
+            <h6 class="d-inline">Rs. 11000</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -81,8 +85,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/ANIMAL PAK 44 PACKS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>ANIMAL PAK 44 PACKS</h6>
-            <h6>Rs. 8500</h6>
+            <h6 class="product-name">ANIMAL PAK 44 PACKS</h6>
+            <h6 class="d-inline">Rs. 8500</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -92,8 +96,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/MUSCLETECH MASS-TECH EXTREME 2000 12 LBS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>MUSCLETECH MASS-TECH</h6>
-            <h6>Rs. 28000</h6>
+            <h6 class="product-name">MUSCLETECH MASS-TECH</h6>
+            <h6 class="d-inline">Rs. 28000</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -103,8 +107,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/CARNIVOR MASS 10 LBS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>CARNIVOR MASS 10 LBS</h6>
-            <h6>Rs. 27500</h6>
+            <h6 class="product-name">CARNIVOR MASS 10 LBS</h6>
+            <h6 class="d-inline">Rs. 27500</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -114,8 +118,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/MUSCLETECH MASS-TECH EXTREME 2000 6 LBS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>MUSCLETECH MASS-TECH</h6>
-            <h6>Rs. 18500</h6>
+            <h6 class="product-name">MUSCLETECH MASS-TECH</h6>
+            <h6 class="d-inline">Rs. 18500</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -125,8 +129,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/MASS INFUSION 12 LBS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>MASS INFUSION 12 LBS</h6>
-            <h6>Rs. 22500</h6>
+            <h6 class="product-name">MASS INFUSION 12 LBS</h6>
+            <h6 class="d-inline">Rs. 22500</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -146,8 +150,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/CARNIVOR MASS 6 LBS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>CARNIVOR MASS 6 LBS</h6>
-            <h6>Rs. 18500</h6>
+            <h6 class="product-name">CARNIVOR MASS 6 LBS</h6>
+            <h6 class="d-inline">Rs. 18500</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -157,8 +161,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/KING MASS XL 15 LBS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>KING MASS XL 15 LBS</h6>
-            <h6>Rs. 29000</h6>
+            <h6 class="product-name">KING MASS XL 15 LBS</h6>
+            <h6 class="d-inline">Rs. 29000</h6>
             <h6 class="text-danger fw-bold ">Out of Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -168,8 +172,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/DYMATIZE ISO 100 5 LBS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>DYMATIZE ISO 100 5 LBS</h6>
-            <h6>Rs. 34500</h6>
+            <h6 class="product-name">DYMATIZE ISO 100 5 LBS</h6>
+            <h6 class="d-inline">Rs. 34500</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -179,8 +183,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/NITROTECH PERFORMANCE SERIES 4 LBS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>NITROTECH PERFORMANCE SERIES</h6>
-            <h6>Rs. 23000</h6>
+            <h6 class="product-name">NITROTECH PERFORMANCE SERIES</h6>
+            <h6 class="d-inline">Rs. 23000</h6>
             <h6 class="text-success fw-bold ">In Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
@@ -190,8 +194,8 @@
         <div class="card border-0  bg-light">
           <img src="Images/products/BPI ISO HD 5 LBS.jpg" class="card-img-top p-2 " alt="...">
           <div class="card-body p-0 pb-2">
-            <h6>BPI ISO HD 5 LBS</h6>
-            <h6>Rs. 26500</h6>
+            <h6 class="product-name">BPI ISO HD 5 LBS</h6>
+            <h6 class="d-inline">Rs. 26500</h6>
             <h6 class="text-danger fw-bold ">Out of Stock</h6>
             <a href="#" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
