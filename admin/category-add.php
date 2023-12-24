6a8f18c0e794f75321865d92ebc88c762415062c
randima217 <152683475+randima217@users.noreply.github.com>
2023-12-24 13:08:05 +0530

    link formValidationScript.js file

--- admin/category-add.php
+++ admin/category-add.php
@@ -34,9 +34,12 @@ mysqli_close($con);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- CSS link -->
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/footer.css">
     <link rel="stylesheet" href="../css/sign-up.css">
+    <!-- JS link -->
+    <script src="formValidationScript.js" defer></script>
 
     <title>Category Add Form-Optimal Nutrition Hub</title>
 </head>
