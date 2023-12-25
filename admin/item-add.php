be5bd7be82240a824a3366a7f98cea1766278de9
randima217 <152683475+randima217@users.noreply.github.com>
2023-12-25 18:26:41 +0530

    create item add form and add som css

---
+++ admin/item-add.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- CSS link -->
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/sign-up.css">
+    <!-- JS link -->
+    <script src="formValidationScript.js" defer></script>
+
+    <title>Item Add Form-Optimal Nutrition Hub</title>
+</head>
+
+<body style="background-color: lightgray;">
+    <!-- Navigation bar start -->
+    <?php
+    include('admin-navigation-bar.php');
+    ?>
+    <!-- Navigation bar end -->
+
+    <!-- item add form section start -->
+    
+    <!-- item add form section end -->
+
+    <!-- Footer section start -->
+    <div class="fixed-bottom ">
+        <footer class="bettle">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-6 col-xs-12">
+                        <p class="copyright-text">Copyright &copy; 2023 Optimal Nutrition Hub | Devoloped by -
+                            <a href="#"> BattleShip</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- Footer section end -->
+
+    <!--Bootstrap JS link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
