d54cb3ee6caf916a85a0fb124ba26db31ccc9949
randima217 <152683475+randima217@users.noreply.github.com>
2024-01-01 11:40:30 +0530

    move search bar codes to separate file

---
+++ includes/search-bar.php
@@ -0,0 +1,6 @@
+<div class="container col-6">
+    <form class="d-flex my-4" role="search">
+        <input class="form-control me-2 search-bar fw-semibold py-1 " type="search" placeholder="Search supplement products here" aria-label="Search">
+        <button class="btn btn-outline-dark    fw-semibold btn-search py-1" type="submit">Search</button>
+    </form>
+</div>
