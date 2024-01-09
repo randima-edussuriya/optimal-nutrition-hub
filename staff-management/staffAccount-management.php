85d9b47cc0fb670d7386dcf9534b06aba949c984
Dilakshana-Rupasinghe <153206022+Dilakshana-Rupasinghe@users.noreply.github.com>
2024-01-09 15:50:02 +0530

    Add BACK button to staff management

--- staff-management/staffAccount-management.php
+++ staff-management/staffAccount-management.php
@@ -20,6 +20,12 @@
     ?>
     <!-- Navigation bar end -->
 
+    <!-- BACK button start -->
+    <div class="back-button-container">
+        <a href="../admin/admin-home.php" class="back-button">Back</a>
+    </div>
+    <!-- BACk button end -->
+
     <!-- Staff details  section start -->
     <h2 class="text-center ">Staff Management</h2>
 
@@ -38,32 +44,32 @@
                 <td>1</td>
                 <td>admin_user</td>
                 <td>Admin</td>
-                <td class="action-buttons" >
+                <td class="action-buttons">
                     <button class="update-button">Update</button>
                     <button class="deactivate-button">Deactivate</button>
                 </td>
             </tr>
 
-    <!-- Staff details section ende -->
+            <!-- Staff details section ende -->
 
-    <!-- Footer section start -->
-    <div class="fixed-bottom ">
-        <footer class="bettle">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-sm-6 col-xs-12">
-                        <p class="copyright-text">Copyright &copy; 2023 Optimal Nutrition Hub | Devoloped by -
-                            <a href="#"> BattleShip</a>
-                        </p>
+            <!-- Footer section start -->
+            <div class="fixed-bottom ">
+                <footer class="bettle">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-sm-6 col-xs-12">
+                                <p class="copyright-text">Copyright &copy; 2023 Optimal Nutrition Hub | Devoloped by -
+                                    <a href="#"> BattleShip</a>
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </footer>
             </div>
-        </footer>
-    </div>
-    <!-- Footer section end -->
+            <!-- Footer section end -->
 
-    <!--Bootstrap JS link -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+            <!--Bootstrap JS link -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
