ba61312d321e0aeb8a7fb066b8f4f5249f0a15f6
randima217 <152683475+randima217@users.noreply.github.com>
2024-01-10 14:52:57 +0530

    optimized code

--- admin/staff-management/staffAccount-management.php
+++ admin/staff-management/staffAccount-management.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../../css/main.css">
     <link rel="stylesheet" href="../../css/footer.css">
-    <link rel="stylesheet" href="../../css/staff-management.css">
+    <link rel="stylesheet" href="../../css/staff-management.css?v=<?php echo time(); ?>">
 
     <title>Sfatt Account Manegement-Optimal Nutrition Hub</title>
 </head>
@@ -54,31 +54,33 @@
                 <td>Admin</td>
                 <td>Active</td>
                 <td class="action-buttons">
-                    <button class="view">View</button>
+                    <button class="view-button">View</button>
                     <button class="update-button">Update</button>
                     <button class="deactivate-button">Deactivate</button>
                 </td>
             </tr>
-            <!-- Staff details section ende -->
+        </tbody>
+    </table>
+    <!-- Staff details section ende -->
 
-            <!-- Footer section start -->
-            <div class="fixed-bottom ">
-                <footer class="bettle">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-8 col-sm-6 col-xs-12">
-                                <p class="copyright-text">Copyright &copy; 2023 Optimal Nutrition Hub | Devoloped by -
-                                    <a href="#"> BattleShip</a>
-                                </p>
-                            </div>
-                        </div>
+    <!-- Footer section start -->
+    <div class="fixed-bottom ">
+        <footer class="bettle">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-6 col-xs-12">
+                        <p class="copyright-text">Copyright &copy; 2023 Optimal Nutrition Hub | Devoloped by -
+                            <a href="#"> BattleShip</a>
+                        </p>
                     </div>
-                </footer>
+                </div>
             </div>
-            <!-- Footer section end -->
+        </footer>
+    </div>
+    <!-- Footer section end -->
 
-            <!--Bootstrap JS link -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!--Bootstrap JS link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
