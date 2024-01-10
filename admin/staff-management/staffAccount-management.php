2b65bab2c24540950500a0e4c7f255085769e0f4
randima217 <152683475+randima217@users.noreply.github.com>
2024-01-11 00:19:57 +0530

    formatted code

--- admin/staff-management/staffAccount-management.php
+++ admin/staff-management/staffAccount-management.php
@@ -5,7 +5,6 @@ include('../../database/config.php');
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -35,14 +34,14 @@ include('../../database/config.php');
     <!-- Staff details  section start -->
     <h2 class="text-center ">Staff Management</h2>
     <table>
-        
-            <!-- get value from staff table and staff type table -->
-            <?php
-            $get_userdetails = "SELECT staff_id, staff_fname, staff_email, staff_username, staff_type_name, staff_is_active FROM staff 
+
+        <!-- get value from staff table and staff type table -->
+        <?php
+        $get_userdetails = "SELECT staff_id, staff_fname, staff_email, staff_username, staff_type_name, staff_is_active FROM staff 
             INNER JOIN staff_type ON staff.fk_staff_type_id = staff_type.staff_type_id";  //get user possition from staff type table 
-            $result = mysqli_query($con, $get_userdetails);
-            $row_count = mysqli_num_rows($result);
-            echo " <tr>
+        $result = mysqli_query($con, $get_userdetails);
+        $row_count = mysqli_num_rows($result);
+        echo " <tr>
             <th>Staff ID</th>
             <th>Name</th>
             <th>Email</th>
@@ -52,28 +51,27 @@ include('../../database/config.php');
             <th>Action</th>
             </tr>";
 
-            // check the table row count 
-            if ($row_count == 0) {
-                echo "<h2 class='bg-danger text-center mt-5 '> No users yet </h2>";
-            } else {
-                $number = 0;
-                while ($row_data = mysqli_fetch_assoc($result)) { //fetch a single row of result data as an associative array
-                    //asign to database valuwe to variables 
-                    $staff_id = $row_data['staff_id'];
-                    $staff_fname = $row_data['staff_fname'];
-                    $staff_email = $row_data['staff_email'];
-                    $staff_username = $row_data['staff_username'];
-                    $staff_type_name = $row_data['staff_type_name'];
-                    $staff_is_active = $row_data['staff_is_active'];
-                    $number++;
+        // check the table row count 
+        if ($row_count == 0) {
+            echo "<h2 class='bg-danger text-center mt-5 '> No users yet </h2>";
+        } else {
+            $number = 0;
+            while ($row_data = mysqli_fetch_assoc($result)) { //fetch a single row of result data as an associative array
+                //asign to database valuwe to variables 
+                $staff_id = $row_data['staff_id'];
+                $staff_fname = $row_data['staff_fname'];
+                $staff_email = $row_data['staff_email'];
+                $staff_username = $row_data['staff_username'];
+                $staff_type_name = $row_data['staff_type_name'];
+                $staff_is_active = $row_data['staff_is_active'];
+                $number++;
 
-                    //check the user is active or deactive
-
-                    $status = "Deactivate"; //defoult value should be deactivate
-                    if ($staff_is_active == 1) {
-                        $status = "Active";
-                    }
-                    echo "<tr>
+                //check the user is active or deactive
+                $status = "Deactivate"; //defoult value should be deactivate
+                if ($staff_is_active == 1) {
+                    $status = "Active";
+                }
+                echo "<tr>
                     <td>$staff_id</td>
                     <td>$staff_fname</td>
                     <td>$staff_email</td>
@@ -87,11 +85,9 @@ include('../../database/config.php');
                     </td>
                 </tr>
                     ";
-                }
             }
-            ?>
-        
-    
+        }
+        ?>
     </table>
     <!-- Staff details section ende -->
 
