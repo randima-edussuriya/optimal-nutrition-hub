ad8fbd6347bffb220f1a0d2a940f5d59bcf2ae12
randima217 <152683475+randima217@users.noreply.github.com>
2024-01-12 14:26:29 +0530

    removed query parameter in css file

--- admin/staff-management/staffAccount-management.php
+++ admin/staff-management/staffAccount-management.php
@@ -13,7 +13,7 @@ include('../../database/config.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../../css/main.css">
     <link rel="stylesheet" href="../../css/footer.css">
-    <link rel="stylesheet" href="../../css/staff-management.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../../css/staff-management.css">
 
     <title>Sfatt Account Manegement-Optimal Nutrition Hub</title>
 </head>
@@ -89,7 +89,6 @@ include('../../database/config.php');
                     </td>
                 </tr> 
                     ";
-                    
             }
         }
         ?>
@@ -97,19 +96,17 @@ include('../../database/config.php');
     <!-- Staff details section ende -->
 
     <!-- Footer section start -->
-    <div class="fixed-bottom ">
-        <footer class="bettle">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-sm-6 col-xs-12">
-                        <p class="copyright-text">Copyright &copy; 2023 Optimal Nutrition Hub | Devoloped by -
-                            <a href="#"> BattleShip</a>
-                        </p>
-                    </div>
+    <footer class="bettle fixed-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12">
+                    <p class="copyright-text">Copyright &copy; 2023 Optimal Nutrition Hub | Devoloped by -
+                        <a href="#"> BattleShip</a>
+                    </p>
                 </div>
             </div>
-        </footer>
-    </div>
+        </div>
+    </footer>
     <!-- Footer section end -->
 
     <!--Bootstrap JS link -->
