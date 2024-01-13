aa602bbe1306ace4a21f227a8f3e944a91da48c0
randima217 <152683475+randima217@users.noreply.github.com>
2024-01-13 10:35:56 +0530

    removed signup from admin login form

--- admin/home-pages/admin-login.php
+++ admin/home-pages/admin-login.php
@@ -90,10 +90,6 @@ if (isset($_POST['staff_login'])) {
                 </div>
                 <!-- login button -->
                 <button type="submit" class="btn text-bg-secondary" name="staff_login"> Login</button>
-                <!-- staff register link -->
-                <div class="register-link">
-                    <p> Don't have an account? <a href="../staff-management/staff-register.php"> Sign-up </a></p>
-                </div>
             </form>
         </div>
     </div>
