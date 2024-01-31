a4778cdc2378fcd74474d64e9cfd4e7a0c87fa77
randima217 <152683475+randima217@users.noreply.github.com>
2024-01-31 12:28:09 +0530

    use number_format method and add comments

--- functions/common-function.php
+++ functions/common-function.php
@@ -11,7 +11,7 @@ function getItemCard($con, $itemSelectQuery)
       $item_sell_price = (float)$itemRow['item_sell_price'];
 
       $discountedPrice = $item_sell_price * (100 - $item_discount) / 100; // discount claculation
-      // show discount if item have discount
+      // show discount if item has discount
       if ($item_discount == 0) {
         $displayNone = 'd-none'; // apply bootstrap d-none class
       } else {
@@ -25,25 +25,28 @@ function getItemCard($con, $itemSelectQuery)
       } else {
         $availabiliy = 'In Stock';
         $AvailabiliyColor = 'text-success'; // apply bootstrap class
-      }
+      } ?>
 
-      // display item card
-      echo "<div class='col-sm-6 col-md-4 col-lg-3 text-center mb-4'>
-        <div class='card border-0 bg-light'>
-          <img src='images/products/{$itemRow['item_image1']}' class='card-img-top p-2 position-relative' alt='{$itemRow['item_name']}'>
-          <span class='$displayNone position-absolute top-0 start-0 rounded-end-pill badge bg-success p-2'>
-            -$item_discount%
+      <!-- display item card -->
+      <div class="col-sm-6 col-md-4 col-lg-3 text-center mb-4">
+        <div class="card border-0 bg-light">
+          <img src="images/products/<?php echo $itemRow['item_image1']; ?>" class="card-img-top p-2 position-relative" alt="<?php echo $itemRow['item_name']; ?>">
+          <!-- item discount percentage-->
+          <span class="<?php echo $displayNone; ?> position-absolute top-0 start-0 rounded-end-pill badge bg-success p-2">
+            -<?php echo $item_discount; ?>%
           </span>
-          <div class='card-body p-0 pb-2'>
-          <h6 class='product-name'>{$itemRow['item_name']}</h6>
-            <h6 class='$displayNone text-decoration-line-through d-inline text-body-tertiary'>Rs. $item_sell_price</h6>
-            <h6 class='d-inline'>Rs. $discountedPrice</h6>
-            <h6 class='$AvailabiliyColor fw-bold '>$availabiliy</h6>
-            <a href='product-view.php?productId={$itemRow['item_id']}' class='btn btn-outline-warning btn-view btn-sm'>View</a>
+          <div class="card-body p-0 pb-2">
+            <h6 class="product-name"><?php echo $itemRow['item_name']; ?></h6>
+            <!-- item prices-->
+            <h6 class="<?php echo $displayNone; ?> text-decoration-line-through d-inline text-body-tertiary">Rs. <?php echo number_format($item_sell_price, 2); ?></h6>
+            <h6 class="d-inline">Rs. <?php echo number_format($discountedPrice, 2); ?></h6>
+            <!-- item availability -->
+            <h6 class="<?php echo $AvailabiliyColor; ?> fw-bold "><?php echo $availabiliy; ?></h6>
+            <a href="product-view.php?productId=<?php echo $itemRow['item_id']; ?>" class="btn btn-outline-warning btn-view btn-sm">View</a>
           </div>
         </div>
-      </div>";
-    }
+      </div>
+<?php }
   } else {
     echo "<h2>Not available products</h2>";
   }
